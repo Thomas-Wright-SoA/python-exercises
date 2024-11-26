4d8df451b891770974c402748605fdea4dbb5128
Thomas-Wright-SoA <Thomas.Wright@schoolofautomation.tech>
2024-11-26 14:53:49 +0000

    Simple calculator
    Prompts the user for a number, a operator, and another number. Performs the calculation, prints the result. After the first calculation, all other calculations are performed using the previous result. Alternatively, enter 'q' to quit the program when prompted for an operator.

---
+++ Additional/exercise2.py
@@ -0,0 +1,39 @@
+def get_number():
+    n = None
+    while True:
+        n = input("Number: ")
+        try:
+            float(n)
+            break
+        except ValueError:
+            continue
+    return n
+
+def get_operator():
+    operator = None
+    while True:
+        operator = input("Operator: ")
+        if operator in ['+', '-', '/', '*']:
+            break
+        elif operator.lower() == 'q':
+            exit()
+    return operator
+
+print("""How to use this calculator:
+      - When prompted for a number enter a number
+      - When prompted for a operator you can choose from the following options
+      -- [+] - Addition
+      -- [-] - Subtraction
+      -- [*] - Multiply
+      -- [/] - Divide
+      -- [q] - Quit""")
+
+result = get_number()
+while True:
+    operator = get_operator()
+    operand = get_number()
+    result = str(eval(result + operator + operand))
+    print(f"Result: {result}")
+
+
+
